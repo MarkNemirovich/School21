4f6f49ba14260fe470fd9156052d8c9269a28458
Mark <2591192@gmail.com>
2024-02-16 15:06:24 +0000

    i and n added

--- String+/s21_sscanf.c
+++ String+/s21_sscanf.c
@@ -178,51 +178,36 @@ int parse_p(char **str, va_list list, flag flags) {
     sign = -1;
     *str += 1;
   }
-  *str += 2; // 0x
-  *p = (void *)(0x0 + sign*get_x_num(str));
+  *str += 2;  // 0x
+  *p = (void *)(0x0 + sign * get_x_num(str));
   *str += 1;
   return error;
 }
 
-int read_types(char **str, va_list list, const char *format, flag flags) {
+int read_types(char **str, va_list list, const char *format, flag flags, const char *start) {
   int error = 0;
-  switch (*format) {
-    case 'c':
-      parse_c(str, list, flags);
-      break;
-    case 's':
-      parse_s(str, list, flags);
-      break;
-    case 'd':
-      parse_d(str, list, flags);
-      break;
-    case 'u':
-      parse_u(str, list, flags);
-      break;
-    case 'x':
-    case 'X':
-      parse_x(str, list, flags);
-      break;
-    case 'o':
-      parse_o(str, list, flags);
-      break;
-    case 'f':
-    case 'e':
-    case 'E':
-    case 'g':
-    case 'G':
-      parse_f(str, list, flags);
-      break;
-    case 'p':
-      parse_p(str, list, flags);
-      break;
-    case '%':
-      *str -= 1;
-      break;
-    default:
-      error = 1;
-      break;
-  }
+  if (*format == 'c')
+    parse_c(str, list, flags);
+  else if (*format == 's')
+    parse_s(str, list, flags);
+  else if (*format == 'x' || *format == 'X' ||
+           *format == 'i' && (*(*str + 2) == 'x' || *(*str + 2) == 'X'))
+    parse_x(str, list, flags);
+  else if (*format == 'o' || *format == 'i' && *(format + 1) == '0')
+    parse_o(str, list, flags);
+  else if (*format == 'd')
+    parse_d(str, list, flags);
+  else if (*format == 'u')
+    parse_u(str, list, flags);
+  else if (*format == 'f' || *format == 'e' || *format == 'E' ||
+           *format == 'g' || *format == 'G')
+    parse_f(str, list, flags);
+  else if (*format == 'p')
+    parse_p(str, list, flags);
+  else if (*format == '%')
+    *str -= 1;
+  else if (*format == 'n')
+    *va_arg(list, long int *) = *str - start;
   format++;
   *str += 1;
   return error;
@@ -233,12 +218,13 @@ int s21_sscanf(const char *str, const char *format, ...) {
   va_start(list, format);
   flag flags = {0};
   int error = 0;
+  const char *start = str;
   while (!error && *str != '\0' && *format != 0) {
     if (*format++ == '%') {
       flags = (flag){0};
       read_flags((char **)&format, list, &flags);
       read_size(format, &flags);
-      error = read_types((char **)&str, list, format, flags);
+      error = read_types((char **)&str, list, format, flags, start);
     }
   }
   va_end(list);
@@ -247,7 +233,7 @@ int s21_sscanf(const char *str, const char *format, ...) {
 
 int main() {
   char i, you, percent;
-  int day, year, oct, hex, HEX, *p;
+  int day, year, oct, hex, HEX, any, *p;
   float a, b, c, d, e, f;
   char at[5], weekday[5], month[5], strings[100], integers[100], fractals[100];
   strcpy(strings, "I at Saturday March ! 25 1989 -40 F aBc");
@@ -255,15 +241,17 @@ int main() {
   strcpy(fractals, "25.1989 -40.5 7e02 9e-02 1.2e+12 4e+102");
 
   s21_sscanf(strings, "%c %s %s %s %c %% %p", &i, at, weekday, month, &you, &p);
-  printf("MY:     \t%c \t%s \t%s \t%s \t%c \t%% \t%p\n", i, at, weekday, month, you, &p);
+  printf("MY:     \t%c \t%s \t%s \t%s \t%c \t%% \t%p\n", i, at, weekday, month,
+         you, &p);
 
   sscanf(strings, "%c %s %s %s %c %% %p", &i, at, weekday, month, &you, &p);
-  printf("BASE:   \t%c \t%s \t%s \t%s \t%c \t%% \t%p\n", i, at, weekday, month, you, &p);
+  printf("BASE:   \t%c \t%s \t%s \t%s \t%c \t%% \t%p\n", i, at, weekday, month,
+         you, &p);
 
-  s21_sscanf(integers, "%d %u %o %x %X", &day, &year, &oct, &hex, &HEX);
-  printf("MY:   \t%d \t%u \t%o \t%x \t%X\n", day, year, oct, hex, HEX);
-  sscanf(integers, "%d %u %o %x %X", &day, &year, &oct, &hex, &HEX);
-  printf("BASE: \t%d \t%u \t%o \t%x \t%X\n", day, year, oct, hex, HEX);
+  s21_sscanf(integers, "%d %u %o %x %X %X", &day, &year, &oct, &hex, &HEX, &any);
+  printf("MY:   \t%d \t%u \t%o \t%x \t%X \t%X\n", day, year, oct, hex, HEX, any);
+  sscanf(integers, "%d %u %o %x %X %X", &day, &year, &oct, &hex, &HEX, &any);
+  printf("BASE: \t%d \t%u \t%o \t%x \t%X \t%X\n", day, year, oct, hex, HEX, any);
 
   s21_sscanf(fractals, "%f %f %e %E %g %G", &a, &b, &c, &d, &e, &f);
   printf("MY:   \t%f \t%f \t%e \t%E \t%g \t%G\n", a, b, c, d, e, f);
